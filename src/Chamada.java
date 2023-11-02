0cb4a2cf5f96065b254e3454e8be1256ee01d925
barbaragrinop <barbarapereira123@hotmail.com>
2023-11-02 19:51:57 -0300

    inicia classe chamada e classe prepago

--- src/Chamada.java
+++ src/Chamada.java
@@ -1,1 +1,15 @@
-public class Chamada {}
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Chamada {
+    private GregorianCalendar data;
+    private int duracao;
+
+    public Chamada(GregorianCalendar data, int duracao) {
+        this.data = data;
+        this.duracao = duracao;
+    }
+
+
+
+}
--- src/PrePago.java
+++ src/PrePago.java
@@ -1,18 +1,50 @@
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class PrePago extends Assinante {
-    private float creditos;
+    private Chamada[] chamadas;
     private int numRecargas;
+    private float creditos;
+
+    public int totalChamadas = 10;
+
 
     public PrePago(long cpf, String nome, int numero){
         super(cpf, nome, numero);
+        this.numRecargas = 0;
+        this.creditos = 0;
+        this.chamadas = new Chamada[totalChamadas];
     }
+
+    public float fazerChamada(GregorianCalendar data, int duracao){
+        Calendar dataAtual = Calendar.getInstance();
+        Chamada c = new Chamada(data, duracao);
+        int qtdChamadas = this.chamadas.length;
+        float valorChamadaAtual = (float)(duracao * 1.45);
+
+
+        if(data.before(dataAtual)){
+            System.out.println("Data precisa ser igual ou posterior ao dia de hoje");
+            return 0f;
+        }
+
+        if(valorChamadaAtual > this.creditos || qtdChamadas == totalChamadas){
+            System.out.println("Chamada não pode ser realizada.");
+            return 0f;
+        }
+
+
+
+
+
+        return 1.12f;
+    }
+
 
     public void recarregar(Date data, float valor){}
 
-    public float fazerChamada(Date data, int duracao){
-        return (float)1.12;
-    }
 
     public void imprimirFatura(int mes){}
 }
