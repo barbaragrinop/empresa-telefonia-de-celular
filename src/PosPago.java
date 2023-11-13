82e3627e37ffebc0ec61494ef5b12408a77ed820
lucasgarcia <lucthebug@gmail.com>
2023-11-13 19:57:48 -0300

    criei o metodo construtor da classe pos pago

--- src/PosPago.java
+++ src/PosPago.java
@@ -1,6 +1,9 @@
 public class PosPago{
-
-    system.out.println("pos pago");
-
-
-}
+    public class PosPago extends Assinante{
+    
+        system.out.println("oi");
+        private float assinatura; 
+    
+        public PosPago(long cpf, String nome, int numero, float assinatura) {
+            super(cpf, nome, numero); 
+            this.assinatura = assinatura;
