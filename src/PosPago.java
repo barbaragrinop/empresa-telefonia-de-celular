9cf763f3699185a299cddcf15a60807cc8c86a7e
lucasgarcia <lucthebug@gmail.com>
2023-11-20 13:49:14 -0300

    criei o método para imprimir a fatura do assinante

--- src/PosPago.java
+++ src/PosPago.java
@@ -1,11 +1,9 @@
 public class PosPago{
     public class PosPago extends Assinante{
-    
-        system.out.println("oi");
+
         private float assinatura; 
     
         public PosPago(long cpf, String nome, int numero, float assinatura) {
             super(cpf, nome, numero); 
             this.assinatura = assinatura;
-
-            System.out.println("oi");
+        
