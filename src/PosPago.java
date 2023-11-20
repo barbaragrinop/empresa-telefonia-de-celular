8965a876788c88d5be3f84016c87e6e6bc175dc3
Lucas Garcia dos Santos <110910228+totebug@users.noreply.github.com>
2023-11-20 14:47:22 -0300

    Update PosPago.java
    criando metodo de realizar chamadas para assinantes

--- src/PosPago.java
+++ src/PosPago.java
@@ -1,9 +1,26 @@
-public class PosPago extends Assinante{
+public class PosPago{
+    public class PosPago extends Assinante{
 
-    private float assinatura; 
+        private float assinatura; 
+    
+        public PosPago(long cpf, String nome, int numero, float assinatura) {
+            super(cpf, nome, numero); 
+            this.assinatura = assinatura;
+        }
 
-    public PosPago(long cpf, String nome, int numero, float assinatura) {
-        super(cpf, nome, numero); 
-        this.assinatura = assinatura;
-    }
-}
+        public void getAssinatura() {
+            return assinatura;
+        }
+    
+        public void setAssinatura(double assinatura) {
+            this.assinatura = assinatura;
+        }
+        
+        public void fazerChamada(GregorianCalendar data, int duracao) {
+            if (numChamadas < chamadas.length) {
+                chamadas[numChamadas] = new Chamada(data, duracao);
+                numChamadas++;
+            } else {
+                System.out.println("Não há espaço para mais chamadas.");
+            }
+        }
