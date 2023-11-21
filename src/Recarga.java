6fd0378236ba4b223993b1ab88f1fe2ea351f087
Lucas Garcia dos Santos <110910228+totebug@users.noreply.github.com>
2023-11-21 10:45:59 -0300

    comecei a criaçao da recarga para prepago

--- src/Recarga.java
+++ src/Recarga.java
@@ -1,1 +1,12 @@
-public class Recarga{}
+public class Recarga{
+    
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+
+public class Recarga {
+
+    private GregorianCalendar data;
+    private float valor;
+
+}
+}
