a69ac81b70148b043a1e13f913aa8d41017f7514
Lucas Garcia dos Santos <110910228+totebug@users.noreply.github.com>
2023-11-21 13:07:59 -0300

    recarga completa

--- src/Recarga.java
+++ src/Recarga.java
@@ -7,6 +7,23 @@ public class Recarga {
 
     private GregorianCalendar data;
     private float valor;
-
+    
+    public Recarga(GregorianCalendar data, float valor) {
+        this.data = data;
+        this.valor = valor;
+    }
+ 
+    public GregorianCalendar getData() {
+        return data;
+    }
+ 
+    public float getValor() {
+        return valor;
+    }
+ 
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return "Data: " + sdf.format(data) + " | Valor: R$" + valor;
+    }
 }
 }
