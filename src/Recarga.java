01496b0f57a5e092c436def70b860627de611572
lucasgarcia <lucthebug@gmail.com>
2023-11-26 12:30:36 -0300

    comentarios da recarga

--- src/Recarga.java
+++ src/Recarga.java
@@ -3,24 +3,32 @@ import java.text.SimpleDateFormat;
 
 public class Recarga {
 
-    private GregorianCalendar data;
-    private float valor;
-    
+    private GregorianCalendar data; 
+    private float valor; 
+
     public Recarga(GregorianCalendar data, float valor) {
+        // construtor da classe recarga
+        // inicializa os campos data e valor com os valores fornecidos
         this.data = data;
         this.valor = valor;
     }
- 
+
     public GregorianCalendar getData() {
+        // retorna o campo data do objeto recarga
+        // o campo data representa a data da recarga
         return data;
     }
- 
+
     public float getValor() {
+        // retorna o campo valor do objeto recarga
+        // o campo valor representa o valor da recarga
         return valor;
     }
- 
+
     public String toString() {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        // sobrescreve o método toString() padrão
+        // retorna uma representação formatada do objeto recarga
         return "Data: " + sdf.format(data) + " | Valor: R$" + valor;
     }
 }
